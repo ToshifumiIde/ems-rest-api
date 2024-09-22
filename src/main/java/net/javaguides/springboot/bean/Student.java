5a9d6020de2bdf2b84332b52810a373acb1a6d92
井手俊文 <toshifumiide198765@gmail.com>
2024-09-22 17:19:39 +0900

    #2 create hello api

---
+++ src/main/java/net/javaguides/springboot/bean/Student.java
@@ -0,0 +1,14 @@
+package net.javaguides.springboot.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Student {
+  private int id;
+  private String firstName;
+  private String lastName;
+}
