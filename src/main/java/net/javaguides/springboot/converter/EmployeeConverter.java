578bb93c796dfe04565d96455e979b73af091c6a
井手俊文 <toshifumiide198765@gmail.com>
2024-09-23 21:35:54 +0900

    #7 edit Converter

--- src/main/java/net/javaguides/springboot/converter/EmployeeConverter.java
+++ src/main/java/net/javaguides/springboot/converter/EmployeeConverter.java
@@ -1,5 +1,6 @@
 package net.javaguides.springboot.converter;
 
+import net.javaguides.springboot.dto.EmployeeReadDto;
 import net.javaguides.springboot.entity.Employee;
 import net.javaguides.springboot.dto.EmployeeRegistrationDto;
 import net.javaguides.springboot.util.UuidGenerate;
@@ -9,17 +10,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class EmployeeConverter {
 
-//  public EmployeeRegistrationDto toDto(Employee employee){
-//    return new EmployeeRegistrationDto(
-//      employee.getId(),
-//      employee.getUuid(),
-//      employee.getFirstName(),
-//      employee.getLastName(),
-//      employee.getEmail()
-//    );
-//  }
+  public EmployeeReadDto toDto(Employee employee) {
+    return new EmployeeReadDto(
+      employee.getUuid(),
+      employee.getFirstName(),
+      employee.getLastName(),
+      employee.getEmail()
+    );
+  }
 
-  public Employee toRegistrationEntity(EmployeeRegistrationDto employeeRegistrationDto){
+  public Employee toRegistrationEntity(EmployeeRegistrationDto employeeRegistrationDto) {
     return new Employee(
       UuidGenerate.generate(),
       employeeRegistrationDto.getFirstName(),
