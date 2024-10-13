aeb45c4de6007b08d68b1a5247c2af8f4c61485b
井手俊文 <toshifumiide198765@gmail.com>
2024-10-13 23:06:46 +0900

    #14 add DepartmentEntity and CepartmentRegistrationDto

---
+++ src/main/java/net/javaguides/springboot/entity/Department.java
@@ -0,0 +1,14 @@
+package net.javaguides.springboot.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Department {
+  private String uuid;
+  private String name;
+  private String description;
+}
