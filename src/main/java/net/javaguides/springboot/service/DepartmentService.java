c7a01669ea70b1b0fceb6077e2ec7f0cc37bafac
井手俊文 <toshifumiide198765@gmail.com>
2024-10-13 23:52:13 +0900

    #15 add getDepartmentByUuid in DepartmentMapper.xml DepartmentRepository DepartmentService DepartmentServiceImpl and add DepartmentReadDto

--- src/main/java/net/javaguides/springboot/service/DepartmentService.java
+++ src/main/java/net/javaguides/springboot/service/DepartmentService.java
@@ -1,7 +1,9 @@
 package net.javaguides.springboot.service;
 
+import net.javaguides.springboot.dto.DepartmentReadDto;
 import net.javaguides.springboot.dto.DepartmentRegistrationDto;
 
 public interface DepartmentService {
   void createDepartment(DepartmentRegistrationDto departmentRegistrationDto);
+  DepartmentReadDto getDepartmentByUuid(String uuid);
 }
