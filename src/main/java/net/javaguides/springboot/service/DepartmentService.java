4268ae8c641e69a8050ee01b7399dc027446680e
井手俊文 <toshifumiide198765@gmail.com>
2024-10-14 07:24:44 +0900

    #20 add deleteDepartmentByUuid in Mapper Repository Service and ServiceImpl

--- src/main/java/net/javaguides/springboot/service/DepartmentService.java
+++ src/main/java/net/javaguides/springboot/service/DepartmentService.java
@@ -14,4 +14,6 @@ public interface DepartmentService {
   DepartmentReadDto getDepartmentByUuid(String uuid);
 
   void updateDepartmentByUuid(String uuid, DepartmentUpdateDto departmentUpdateDto);
+
+  void deleteDepartmentByUuid(String uuid);
 }
