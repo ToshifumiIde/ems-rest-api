08099897dd6eee29d98d5c7b7fd9241e5287a3e3
井手俊文 <toshifumiide198765@gmail.com>
2024-10-13 23:58:28 +0900

    #15 add getByUuid in DepartmentController

--- src/main/java/net/javaguides/springboot/controller/DepartmentController.java
+++ src/main/java/net/javaguides/springboot/controller/DepartmentController.java
@@ -1,10 +1,14 @@
 package net.javaguides.springboot.controller;
 
 import lombok.RequiredArgsConstructor;
+import net.javaguides.springboot.dto.DepartmentReadDto;
 import net.javaguides.springboot.dto.DepartmentRegistrationDto;
 import net.javaguides.springboot.service.DepartmentService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,5 +33,17 @@ public class DepartmentController {
   public ResponseEntity<Void> create(@RequestBody DepartmentRegistrationDto departmentRegistrationDto) {
     departmentService.createDepartment(departmentRegistrationDto);
     return ResponseEntity.ok().build();
+  }
+
+  /**
+   * UUIDを指定してDepartmentを取得するREST API
+   *
+   * @param uuid 部署UUID
+   * @return DepartmentReadDto 部署取得用DTO
+   */
+  @GetMapping("{uuid}")
+  public ResponseEntity<DepartmentReadDto> getByUuid(@PathVariable("uuid") String uuid) {
+    DepartmentReadDto dto = departmentService.getDepartmentByUuid(uuid);
+    return new ResponseEntity<>(dto, HttpStatus.OK);
   }
 }
