febf31739b63bc0567e8b6771b74c0c57c61b8f0
井手俊文 <toshifumiide198765@gmail.com>
2024-10-19 15:31:03 +0900

    #21 fix EmployeeService

--- src/main/java/net/javaguides/springboot/service/EmployeeService.java
+++ src/main/java/net/javaguides/springboot/service/EmployeeService.java
@@ -3,7 +3,6 @@ package net.javaguides.springboot.service;
 import net.javaguides.springboot.dto.EmployeeReadDto;
 import net.javaguides.springboot.dto.EmployeeRegistrationDto;
 import net.javaguides.springboot.dto.EmployeeUpdateDto;
-import net.javaguides.springboot.entity.Employee;
 
 import java.util.List;
 
